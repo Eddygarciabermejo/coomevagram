c975ee3c7659b286a2256023a73349a1ddf45928
cesardramirez <cesardavid89@gmail.com>
2019-01-29 13:44:15 -0500

    feat(bootstrap): Crear template con bootstrap

--- posts/views.py
+++ posts/views.py
@@ -1,45 +1,39 @@
 """ Posts views """
 
-from django.http import HttpResponse
+from django.shortcuts import render
 
 from datetime import datetime
 
 posts = [
     {
-        'name': 'Mont Blac',
-        'user': 'Yésica Cortés',
+        'title': 'Mont Blac',
+        'user': {
+            'name': 'Yésica Cortés',
+            'picture': 'https://picsum.photos/60/60/?image=1027'
+        },
         'timestamp': datetime.now().strftime('%b %dth, %Y - %H:%M hrs'),
-        'picture': 'https://picsum.photos/200/200/?image=1036'
+        'photo': 'https://picsum.photos/800/600/?image=1036'
     },
     {
-        'name': 'Via Láctea',
-        'user': 'C. Vander',
+        'title': 'Via Láctea',
+        'user': {
+            'name': 'Christian Van der Henst',
+            'picture': 'https://picsum.photos/60/60/?image=1005'
+        },
         'timestamp': datetime.now().strftime('%b %dth, %Y - %H:%M hrs'),
-        'picture': 'https://picsum.photos/200/200/?image=903'
+        'photo': 'https://picsum.photos/800/800/?image=903'
     },
     {
-        'name': 'Nuevo auditorio',
-        'user': 'Thepianartist',
+        'title': 'Nuevo auditorio',
+        'user': {
+            'name': 'Uriel (thespianartist)',
+            'picture': 'https://picsum.photos/60/60/?image=883'
+        },
         'timestamp': datetime.now().strftime('%b %dth, %Y - %H:%M hrs'),
-        'picture': 'https://picsum.photos/200/200/?image=1076'
+        'photo': 'https://picsum.photos/500/700/?image=1076'
     }
 ]
 
 
 def list_posts(request):
-    """
-    List existing posts with images.
-    :param request:
-    :return: content html
-    """
-    content = []
-
-    for post in posts:
-        content.append("""
-            <p><strong>{name}</stong></p>
-            <p><small>{user} - <i>{timestamp}</i></small></p>
-            <figure><img src="{picture}"/></figure>
-        """.format(**post))
-
-    # Por cada iteración se concatenará un <br>.
-    return HttpResponse('<br>'.join(content))
+    return render(request, 'feed.html', {'posts': posts})
