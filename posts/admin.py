4c6fa02d8bb1308a51bc74254b92c727becaf6e7
cesardramirez <cesardavid89@gmail.com>
2019-02-10 22:31:07 -0500

    feat(admin): Agregar enlace de User desde Post.

--- posts/admin.py
+++ posts/admin.py
@@ -1,3 +1,24 @@
 from django.contrib import admin
+from django.urls import reverse
+from django.utils.safestring import mark_safe
+
+from posts.models import Post
 
 
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    """ Post admin """
+
+    list_display = ('pk', 'user_link', 'title', 'photo')
+    readonly_fields = ('user_link', )
+    list_editable = ('title', )
+    search_fields = ('title', 'photo', 'user__username')
+    list_filter = ('created', 'modified')
+
+    def user_link(self, obj):
+        return mark_safe('<a href="{}">{}</a>'.format(
+            reverse("admin:auth_user_change", args=(obj.user.pk,)),
+            obj.user.username
+        ))
+
+    user_link.short_description = 'user'
