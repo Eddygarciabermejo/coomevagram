0469c8447bad86164fbea4abe8094d098c71667b
cesardramirez <cesardavid89@gmail.com>
2019-03-12 22:13:07 -0500

    feat(template-view): Agregar template detail para un usuario

--- users/urls.py
+++ users/urls.py
@@ -1,9 +1,15 @@
 """ Users URLs """
 
 from django.urls import path
+from django.views.generic import TemplateView
+
 from users import views
 
 urlpatterns = [
+    # Posts
+    path(route='<str:username>/', view=TemplateView.as_view(template_name='users/detail.html'), name='detail'),
+
+    # Management
     path(route='login/', view=views.login_view, name="login"),
     path(route='logout/', view=views.logout_view, name="logout"),
     path(route='signup/', view=views.signup_view, name="signup"),
